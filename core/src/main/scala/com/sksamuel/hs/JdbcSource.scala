3564e7def931471ed27a0ac5c7a0ba47bc0ba693
sksamuel <sam@sksamuel.com>
2016-01-25 11:59:01 +0000

    Added fetch size to jdbc source

--- core/src/main/scala/com/sksamuel/hs/JdbcSource.scala
+++ core/src/main/scala/com/sksamuel/hs/JdbcSource.scala
@@ -2,11 +2,17 @@ package com.sksamuel.hs
 
 import java.sql.DriverManager
 
-case class JdbcSource(url: String, query: String) extends Source {
+case class JdbcSource(url: String, query: String, props: JdbcSourceProps = JdbcSourceProps(100)) extends Source {
   override def loader: Iterator[Seq[String]] = new Iterator[Seq[String]] {
     lazy val conn = DriverManager.getConnection(url)
-    lazy val rs = conn.createStatement().executeQuery(query)
+    lazy val rs = {
+      val stmt = conn.createStatement()
+      stmt.setFetchSize(props.fetchSize)
+      stmt.executeQuery(query)
+    }
     override def hasNext: Boolean = rs.next()
     override def next(): Seq[String] = for ( k <- 1 to rs.getMetaData.getColumnCount ) yield rs.getString(k)
   }
 }
+
+case class JdbcSourceProps(fetchSize: Int)
