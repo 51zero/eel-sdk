80cbd68ae273674be7c49314d2dae503831e0e2f
sksamuel <sam@sksamuel.com>
2016-02-02 07:17:08 +0000

    Fix for parquet source fixes

--- components/parquet/src/main/scala/io/eels/component/parquet/ParquetSource.scala
+++ components/parquet/src/main/scala/io/eels/component/parquet/ParquetSource.scala
@@ -4,6 +4,7 @@ import com.typesafe.scalalogging.slf4j.StrictLogging
 import io.eels.{Column, Field, FilePattern, Reader, Row, Source}
 import org.apache.avro.generic.GenericRecord
 import org.apache.parquet.avro.AvroParquetReader
+import org.apache.parquet.hadoop.ParquetReader
 
 import scala.collection.JavaConverters._
 
@@ -13,7 +14,7 @@ case class ParquetSource(pattern: FilePattern) extends Source with StrictLogging
 
     private val paths = pattern.toPaths
     logger.debug(s"Parquet source will read from $paths")
-    private val readers = paths.map(AvroParquetReader.builder[GenericRecord](_).build().asInstanceOf[AvroParquetReader[GenericRecord]])
+    private val readers = paths.map(AvroParquetReader.builder[GenericRecord](_).build().asInstanceOf[ParquetReader[GenericRecord]])
 
     private val iterators = readers.map(reader => Iterator.continually(reader.read).takeWhile(_ != null).map { record =>
       val columns = record.getSchema.getFields.asScala.map(_.name).map(Column.apply)
