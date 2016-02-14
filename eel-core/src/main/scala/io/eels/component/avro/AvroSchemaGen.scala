e1cb3c759202eb5aff8f5ec17cffc5bf9fb8e117
sksamuel <sam@sksamuel.com>
2016-02-14 23:11:17 +0000

    Fixed warning in avro schema gen

--- eel-core/src/main/scala/io/eels/component/avro/AvroSchemaGen.scala
+++ eel-core/src/main/scala/io/eels/component/avro/AvroSchemaGen.scala
@@ -1,10 +1,11 @@
 package io.eels.component.avro
 
+import com.typesafe.scalalogging.slf4j.StrictLogging
 import io.eels.{SchemaType, Column, FrameSchema}
 import org.apache.avro.Schema
 import scala.collection.JavaConverters._
 
-object AvroSchemaGen {
+object AvroSchemaGen extends StrictLogging {
 
   def apply(fs: FrameSchema): Schema = {
     val schema = Schema.createRecord("row", "", "io.eels.avro.generated", false)
@@ -20,5 +21,8 @@ object AvroSchemaGen {
     case SchemaType.Double => new Schema.Field(column.name, Schema.create(Schema.Type.DOUBLE), "", null)
     case SchemaType.Float => new Schema.Field(column.name, Schema.create(Schema.Type.FLOAT), "", null)
     case SchemaType.Long => new Schema.Field(column.name, Schema.create(Schema.Type.LONG), "", null)
+    case other =>
+      logger.warn("Unknown column type; defaulting to string")
+      new Schema.Field(column.name, Schema.create(Schema.Type.STRING), "", null)
   }
 }
