65a701f36b42cd42f7af07c419411aec3bdecae5
sksamuel <sam@sksamuel.com>
2016-02-15 23:07:05 +0000

    Split hive file explorer into partitioned and non partitioned implementations

--- eel-core/src/main/scala/io/eels/component/hive/HiveFileExplorer.scala
+++ eel-core/src/main/scala/io/eels/component/hive/HiveFileExplorer.scala
@@ -6,19 +6,46 @@ import io.eels.HdfsIterator
 import org.apache.hadoop.fs.{FileSystem, LocatedFileStatus, Path}
 import org.apache.hadoop.hive.metastore.api.Table
 
-object HiveFileExplorer extends StrictLogging {
+object HiveFileExplorer {
+  def apply(table: Table, partitionExprs: List[PartitionExpr])(implicit fs: FileSystem): List[Path] = {
+    if (partitionExprs.isEmpty) TableLocationHiveFileExplorer(table, partitionExprs)
+    else PartitionHiveFileExplorer(table, partitionExprs)
+  }
+}
 
-  val config = ConfigFactory.load()
-  val ignoreHiddenFiles = config.getBoolean("eel.hive.source.ignoreHiddenFiles")
-  val hiddenFilePattern = config.getString("eel.hive.source.hiddenFilePattern")
+trait HiveFileExplorer extends StrictLogging {
 
-  def apply(table: Table, partitionExprs: List[PartitionExpr])
-           (implicit fs: FileSystem): List[Path] = {
+  protected val config = ConfigFactory.load()
+  protected val ignoreHiddenFiles = config.getBoolean("eel.hive.source.ignoreHiddenFiles")
+  protected val hiddenFilePattern = config.getString("eel.hive.source.hiddenFilePattern")
 
-    // returns true if the given file should be considered "hidden" based on the config settings
-    def isHidden(file: LocatedFileStatus): Boolean = {
-      ignoreHiddenFiles && file.getPath.getName.matches(hiddenFilePattern)
-    }
+  def apply(table: Table, partitionExprs: List[PartitionExpr])(implicit fs: FileSystem): List[Path]
+
+  // returns true if the given file should be considered "hidden" based on the config settings
+  def isHidden(file: LocatedFileStatus): Boolean = {
+    ignoreHiddenFiles && file.getPath.getName.matches(hiddenFilePattern)
+  }
+
+  // returns all files in the given location that are visible
+  def scan(location: String)(implicit fs: FileSystem): List[LocatedFileStatus] = {
+    logger.debug(s"Scanning $location, filtering=$ignoreHiddenFiles, pattern=$hiddenFilePattern")
+    val files = HdfsIterator(fs.listFiles(new Path(location), true)).filter(_.isFile).filterNot(isHidden).toList
+    logger.debug(s"Found ${files.size} visible files")
+    files
+  }
+}
+
+// loads all files under the table location
+object TableLocationHiveFileExplorer extends HiveFileExplorer {
+  override def apply(table: Table, partitionExprs: List[PartitionExpr])(implicit fs: FileSystem): List[Path] = {
+    val location = table.getSd.getLocation
+    scan(location).map(_.getPath)
+  }
+}
+
+object PartitionHiveFileExplorer extends HiveFileExplorer {
+
+  override def apply(table: Table, partitionExprs: List[PartitionExpr])(implicit fs: FileSystem): List[Path] = {
 
     // returns true if the file meets the partition exprs
     def isEvaluated(file: LocatedFileStatus): Boolean = {
@@ -33,19 +60,10 @@ object HiveFileExplorer extends StrictLogging {
       partitionExprs.forall(_.eval(partitions))
     }
 
+    val location = table.getSd.getLocation
+
     // returns all files in the given location that are both visible and that match the partition expressions
     // todo this doesn't take into account partitions that are not stored under the table location
-    def paths(location: String): List[Path] = {
-      logger.debug(s"Scanning $location, filtering=$ignoreHiddenFiles, pattern=$hiddenFilePattern")
-      val files = HdfsIterator(fs.listFiles(new Path(location), true)).filter(_.isFile).toList
-      logger.debug(s"Found ${files.size} files before filtering")
-
-      val visible = files.filterNot(isHidden).filter(isEvaluated).map(_.getPath)
-      logger.info(s"Found ${visible.size} files after filtering")
-      visible
-    }
-
-    val location = table.getSd.getLocation
-    paths(location)
+    scan(location).filter(isEvaluated).map(_.getPath)
   }
 }
