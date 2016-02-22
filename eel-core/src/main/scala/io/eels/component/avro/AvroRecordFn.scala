b3aa7b0cdcbf92659e4728eb8458288e334859dd
sksamuel <sam@sksamuel.com>
2016-02-22 23:38:07 +0000

    Added config key to replace missing values with null in avro #82

--- eel-core/src/main/scala/io/eels/component/avro/AvroRecordFn.scala
+++ eel-core/src/main/scala/io/eels/component/avro/AvroRecordFn.scala
@@ -1,13 +1,19 @@
 package io.eels.component.avro
 
+import com.sksamuel.scalax.Logging
+import com.typesafe.config.ConfigFactory
 import io.eels.{FrameSchema, InternalRow}
 import org.apache.avro.Schema
 import org.apache.avro.generic.GenericData.Record
 import org.apache.avro.generic.GenericRecord
 
-object AvroRecordFn {
+import scala.collection.JavaConverters._
 
-  import scala.collection.JavaConverters._
+object AvroRecordFn extends Logging {
+
+  val config = ConfigFactory.load()
+  val replaceMissing = config.getBoolean("eel.avro.fillMissingValues")
+  logger.debug(s"Avro records replaceMissing=$replaceMissing")
 
   def fromRecord(record: GenericRecord): InternalRow = {
     record.getSchema.getFields.asScala.map { field =>
@@ -26,9 +32,17 @@ object AvroRecordFn {
     * to determine the correct ordering from the row.
     */
   def toRecord(row: InternalRow, avroSchema: Schema, sourceSchema: FrameSchema): GenericRecord = {
+
+    def default(field: Schema.Field) = {
+      if (replaceMissing) null
+      else sys.error(s"Record is missing value for column $field")
+    }
+
+    val map = sourceSchema.columnNames.zip(row).toMap
     val record = new Record(avroSchema)
-    for ((columnName, value) <- sourceSchema.columnNames.zip(row)) {
-      record.put(columnName, value)
+    for (field <- avroSchema.getFields.asScala) {
+      val value = map.getOrElse(field.name, default(field))
+      record.put(field.name, value)
     }
     record
   }
