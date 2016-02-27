a4d1dd7f505ef4892eeaaf5db5a98e874d19df8c
sksamuel <sam@sksamuel.com>
2016-02-27 06:27:34 +0000

    Fixed sql context test

--- eel-core/src/main/scala/io/eels/SqlContext.scala
+++ eel-core/src/main/scala/io/eels/SqlContext.scala
@@ -12,10 +12,11 @@ class SqlContext {
 
   val config = ConfigFactory.load()
   val disk = config.getBoolean("eel.sqlContext.writeToDisk")
+  val dataDirectory = config.getString("eel.sqlContext.dataDirectory")
   val ignoreCase = config.getBoolean("eel.sqlContext.ignoreCase").toString.toUpperCase
 
   val uri = if (disk) {
-    s"jdbc:h2:sqlcontext${UUID.randomUUID.toString.replace("-", "")};IGNORECASE=$ignoreCase;DB_CLOSE_DELAY=-1"
+    s"jdbc:h2:$dataDirectory/sqlcontext${UUID.randomUUID.toString.replace("-", "")};IGNORECASE=$ignoreCase;DB_CLOSE_DELAY=-1"
   } else {
     s"jdbc:h2:mem:sqlcontext${UUID.randomUUID.toString.replace("-", "")};IGNORECASE=$ignoreCase;DB_CLOSE_DELAY=-1"
   }
