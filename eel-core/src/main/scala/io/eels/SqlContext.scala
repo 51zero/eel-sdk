f333ec31a0d05225ca3db4255d8625411a0f6c08
sksamuel <sam@sksamuel.com>
2016-02-27 05:07:29 +0000

    Allow SQLContext to write to disk #80

--- eel-core/src/main/scala/io/eels/SqlContext.scala
+++ eel-core/src/main/scala/io/eels/SqlContext.scala
@@ -2,14 +2,23 @@ package io.eels
 
 import java.util.UUID
 
-import io.eels.component.jdbc.{JdbcSinkProps, JdbcSink, JdbcSource}
+import com.typesafe.config.ConfigFactory
+import io.eels.component.jdbc.{JdbcSink, JdbcSinkProps, JdbcSource}
 
 import scala.concurrent.ExecutionContext
 
 class SqlContext {
-
   Class.forName("org.h2.Driver")
-  val uri = s"jdbc:h2:mem:sqlcontext${UUID.randomUUID.toString.replace("-", "")};IGNORECASE=TRUE;DB_CLOSE_DELAY=-1"
+
+  val config = ConfigFactory.load()
+  val disk = config.getBoolean("eel.sqlContext.writeToDisk")
+  val ignoreCase = config.getBoolean("eel.sqlContext.ignoreCase").toString.toUpperCase
+
+  val uri = if (disk) {
+    s"jdbc:h2:sqlcontext${UUID.randomUUID.toString.replace("-", "")};IGNORECASE=$ignoreCase;DB_CLOSE_DELAY=-1"
+  } else {
+    s"jdbc:h2:mem:sqlcontext${UUID.randomUUID.toString.replace("-", "")};IGNORECASE=$ignoreCase;DB_CLOSE_DELAY=-1"
+  }
 
   def registerFrame(name: String, frame: Frame)(implicit executionContext: ExecutionContext): Unit = {
     frame.to(JdbcSink(uri, name, JdbcSinkProps(createTable = true)))
