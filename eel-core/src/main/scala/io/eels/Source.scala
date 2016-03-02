bb24db76ad1f280cc6aa9cfd3b2aa6d1db90a5cf
sksamuel <sam@sksamuel.com>
2016-03-02 07:55:09 +0000

    Added config value for source buffer size

--- eel-core/src/main/scala/io/eels/Source.scala
+++ eel-core/src/main/scala/io/eels/Source.scala
@@ -3,7 +3,9 @@ package io.eels
 import java.util.concurrent.atomic.AtomicLong
 import java.util.concurrent._
 
+import com.sksamuel.scalax.Logging
 import com.sksamuel.scalax.collection.BlockingQueueConcurrentIterator
+import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.slf4j.StrictLogging
 
 import scala.language.implicitConversions
@@ -11,75 +13,78 @@ import scala.language.implicitConversions
 trait Source extends StrictLogging {
   self =>
 
-  val DefaultBufferSize = 1000
-
   def schema: Schema
   def readers: Seq[Reader]
 
-  def toFrame(ioThreads: Int): Frame = new Frame {
+  def toFrame(ioThreads: Int): Frame = new FrameSource(ioThreads, this)
+}
 
-    override lazy val schema: Schema = self.schema
+class FrameSource(ioThreads: Int, source: Source) extends Frame with Logging {
 
-    override def buffer: Buffer = {
-      import com.sksamuel.scalax.concurrent.ExecutorImplicits._
+  private val config = ConfigFactory.load()
+  private val bufferSize = config.getInt("eel.source.defaultBufferSize")
 
-      val executor = Executors.newFixedThreadPool(ioThreads)
-      logger.debug(s"Source will read using $ioThreads io threads")
+  override lazy val schema: Schema = source.schema
 
-      try {
+  override def buffer: Buffer = {
+    import com.sksamuel.scalax.concurrent.ExecutorImplicits._
 
-        val readers = self.readers
-        logger.debug(s"Source has ${readers.size} reader(s)")
+    val executor = Executors.newFixedThreadPool(ioThreads)
+    logger.debug(s"Source will read using $ioThreads io threads")
 
-        // faster to use a linked blocking queue than an array one
-        // as maybe the array impl is copying the array and not doing some clever position pointer stuff
-        val queue = new LinkedBlockingQueue[InternalRow](DefaultBufferSize)
-        val count = new AtomicLong(0)
-        val latch = new CountDownLatch(readers.size)
+    try {
 
-        for ( reader <- readers ) {
-          executor.submit {
-            try {
-              reader.iterator.foreach(queue.put)
-              logger.debug(s"Completed reader #${count.incrementAndGet}")
-            } catch {
-              case e: Throwable =>
-                logger.error("Error reading row; aborting", e)
-            } finally {
-              latch.countDown()
-              reader.close()
-            }
-          }
-        }
+      val readers = source.readers
+      logger.debug(s"Source has ${readers.size} reader(s)")
 
+      // faster to use a linked blocking queue than an array one
+      // as maybe the array impl is copying the array and not doing some clever position pointer stuff
+      val queue = new LinkedBlockingQueue[InternalRow](bufferSize)
+      val count = new AtomicLong(0)
+      val latch = new CountDownLatch(readers.size)
+
+      for (reader <- readers) {
         executor.submit {
-          latch.await(1, TimeUnit.DAYS)
-          logger.debug("Readers completed; latch released")
           try {
-            queue.put(InternalRow.PoisonPill)
+            reader.iterator.foreach(queue.put)
+            logger.debug(s"Completed reader #${count.incrementAndGet}")
           } catch {
             case e: Throwable =>
-              logger.error("Error adding sentinel", e)
+              logger.error("Error reading row; aborting", e)
+          } finally {
+            latch.countDown()
+            reader.close()
           }
-          logger.debug("Sentinel added to downstream queue from source")
         }
-
-        executor.shutdown()
-
-        new Buffer {
-          override def close(): Unit = {
-            logger.debug("Closing source")
-            executor.shutdownNow()
-          }
-          override def iterator: Iterator[InternalRow] = BlockingQueueConcurrentIterator(queue, InternalRow.PoisonPill)
-        }
-
-      } catch {
-        case t: Throwable =>
-          logger.error("Error opening readers", t)
-          executor.shutdownNow()
-          throw t
       }
+
+      executor.submit {
+        latch.await(1, TimeUnit.DAYS)
+        logger.debug("Readers completed; latch released")
+        try {
+          queue.put(InternalRow.PoisonPill)
+        } catch {
+          case e: Throwable =>
+            logger.error("Error adding sentinel", e)
+        }
+        logger.debug("Sentinel added to downstream queue from source")
+      }
+
+      executor.shutdown()
+
+      new Buffer {
+        override def close(): Unit = {
+          logger.debug("Closing source")
+          executor.shutdownNow()
+        }
+        override def iterator: Iterator[InternalRow] = BlockingQueueConcurrentIterator(queue, InternalRow.PoisonPill)
+      }
+
+    } catch {
+      case t: Throwable =>
+        logger.error("Error opening readers", t)
+        executor.shutdownNow()
+        throw t
     }
   }
 }
