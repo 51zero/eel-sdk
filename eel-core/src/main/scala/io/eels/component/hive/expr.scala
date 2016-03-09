79b035585f05f1cc3540d08ce4e6bfcc61980919
sksamuel <sam@sksamuel.com>
2016-03-09 10:00:23 +0000

    Changed .withPartitionConstraint to accept typeclass #130

--- eel-core/src/main/scala/io/eels/component/hive/expr.scala
+++ eel-core/src/main/scala/io/eels/component/hive/expr.scala
@@ -1,33 +1,35 @@
 package io.eels.component.hive
 
-trait PartitionExpr {
-  // returns true if this expr matches one of the given partition parts
+trait PartitionConstraint {
+  // returns true if the partition name and value matches one of the given partition parts
   def eval(partitions: Seq[PartitionPart]): Boolean
 }
 
-case class PartitionEquals(name: String, value: String) extends PartitionExpr {
-  override def eval(partitions: Seq[PartitionPart]): Boolean = partitions.contains(PartitionPart(name, value))
+case class PartitionEquals(name: String, value: String) extends PartitionConstraint {
+  override def eval(partitions: Seq[PartitionPart]): Boolean = {
+    partitions.contains(PartitionPart(name, value))
+  }
 }
 
-case class PartitionLt(name: String, value: String) extends PartitionExpr {
+case class PartitionLt(name: String, value: String) extends PartitionConstraint {
   override def eval(partitions: Seq[PartitionPart]): Boolean = {
     partitions.find(_.key == name).exists(_.value.compareTo(value) < 0)
   }
 }
 
-case class PartitionLte(name: String, value: String) extends PartitionExpr {
+case class PartitionLte(name: String, value: String) extends PartitionConstraint {
   override def eval(partitions: Seq[PartitionPart]): Boolean = {
     partitions.find(_.key == name).exists(_.value.compareTo(value) <= 0)
   }
 }
 
-case class PartitionGt(name: String, value: String) extends PartitionExpr {
+case class PartitionGt(name: String, value: String) extends PartitionConstraint {
   override def eval(partitions: Seq[PartitionPart]): Boolean = {
     partitions.find(_.key == name).exists(_.value.compareTo(value) > 0)
   }
 }
 
-case class PartitionGte(name: String, value: String) extends PartitionExpr {
+case class PartitionGte(name: String, value: String) extends PartitionConstraint {
   override def eval(partitions: Seq[PartitionPart]): Boolean = {
     partitions.find(_.key == name).exists(_.value.compareTo(value) >= 0)
   }
