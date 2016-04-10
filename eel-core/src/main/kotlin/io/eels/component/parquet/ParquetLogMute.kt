219588278a4a8f0130a781941f87384da31ad148
sksamuel <sam@sksamuel.com>
2016-04-10 16:20:13 +0100

    Migrated parquet log mute

---
+++ eel-core/src/main/kotlin/io/eels/component/parquet/ParquetLogMute.kt
@@ -0,0 +1,25 @@
+package io.eels.component.parquet
+
+import java.util.logging.Logger
+
+fun parquetLogMute(): Unit {
+
+  try {
+    Class.forName("org.apache.parquet.Log")
+  } catch(e: Exception) {
+  }
+
+  try {
+    Class.forName("parquet.Log")
+  } catch(e: Exception) {
+  }
+
+  for (pack in listOf("org.apache.parquet", "parquet")) {
+    try {
+      val logger = Logger.getLogger(pack)
+      logger.handlers.forEach { logger.removeHandler(it) }
+      logger.useParentHandlers = false
+    } catch(e: Exception) {
+    }
+  }
+}
