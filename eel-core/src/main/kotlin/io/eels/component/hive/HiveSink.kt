a906664494b73d4623ee04606375e446c9ff010e
sksamuel <sam@sksamuel.com>
2016-08-04 22:39:22 +0100

    Updated hive sink to avoid Option to make it better compatible with java

--- eel-core/src/main/kotlin/io/eels/component/hive/HiveSink.kt
+++ eel-core/src/main/kotlin/io/eels/component/hive/HiveSink.kt
@@ -6,8 +6,6 @@ import io.eels.util.Logging
 import io.eels.schema.Schema
 import io.eels.Sink
 import io.eels.SinkWriter
-import io.eels.util.Option
-import io.eels.util.getOrElse
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.hive.conf.HiveConf
@@ -19,20 +17,20 @@ data class HiveSink @JvmOverloads constructor(private val dbName: String,
                                               val fs: FileSystem = FileSystem.get(Configuration()),
                                               val client: IMetaStoreClient = HiveMetaStoreClient(HiveConf()),
                                               private val ioThreads: Int = 4,
-                                              private val dynamicPartitioning: Option<Boolean> = Option.None,
-                                              private val schemaEvolution: Option<Boolean> = Option.None) : Sink, Logging {
+                                              private val dynamicPartitioning: Boolean? = null,
+                                              private val schemaEvolution: Boolean? = null) : Sink, Logging {
 
   val config: Config = ConfigFactory.load()
   val includePartitionsInData = config.getBoolean("eel.hive.includePartitionsInData")
   val bufferSize = config.getInt("eel.hive.bufferSize")
-  val SchemaEvolutionDefault = config.getBoolean("eel.hive.sink.schemaEvolution")
-  val DynamicPartitioningDefault = config.getBoolean("eel.hive.sink.dynamicPartitioning")
+  val schemaEvolutionDefault = config.getBoolean("eel.hive.sink.schemaEvolution")
+  val dynamicPartitioningDefault = config.getBoolean("eel.hive.sink.dynamicPartitioning")
 
   val ops = HiveOps(client)
 
   fun withIOThreads(ioThreads: Int): HiveSink = copy(ioThreads = ioThreads)
-  fun withDynamicPartitioning(partitioning: Boolean): HiveSink = copy(dynamicPartitioning = Option(partitioning))
-  fun withSchemaEvolution(schemaEvolution: Boolean): HiveSink = copy(schemaEvolution = Option(schemaEvolution))
+  fun withDynamicPartitioning(partitioning: Boolean): HiveSink = copy(dynamicPartitioning = partitioning)
+  fun withSchemaEvolution(schemaEvolution: Boolean): HiveSink = copy(schemaEvolution = schemaEvolution)
 
   private fun dialect(): HiveDialect {
     val format = ops.tableFormat(dbName, tableName)
@@ -47,7 +45,7 @@ data class HiveSink @JvmOverloads constructor(private val dbName: String,
       logger.warn("Writing to hive with a schema that contains upper case characters is discouraged because Hive will lowercase all the values. This might lead to subtle case bugs. It is recommended, but not required, that you explicitly convert schemas to lower case before serializing to hive")
     }
 
-    if (schemaEvolution.getOrElse(SchemaEvolutionDefault)) {
+    if (schemaEvolution ?: schemaEvolutionDefault) {
       // HiveSchemaEvolve(dbName, tableName, schema)
       throw UnsupportedOperationException("Schema evolution is not yet implemented")
     }
@@ -61,7 +59,7 @@ data class HiveSink @JvmOverloads constructor(private val dbName: String,
         tableName,
         ioThreads,
         dialect(),
-        dynamicPartitioning.getOrElse(DynamicPartitioningDefault),
+        dynamicPartitioning ?: dynamicPartitioningDefault,
         includePartitionsInData,
         bufferSize,
         fs,
