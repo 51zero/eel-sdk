dcee1227e568f9026df9a17a77b6d14ce9385781
sksamuel <sam@sksamuel.com>
2016-08-15 20:08:12 +0100

    csv test migration

---
+++ eel-core/src/test/scala/io/eels/component/csv/CsvSinkTest.scala
@@ -0,0 +1,67 @@
+package io.eels.component.csv
+
+import java.nio.charset.Charset
+import java.nio.file.Files
+
+import io.eels.{Frame, Row}
+import io.eels.schema.{Field, Schema}
+import org.scalatest.{Matchers, WordSpec}
+import scala.collection.JavaConverters._
+
+class CsvSinkTest extends WordSpec with Matchers {
+
+  "CsvSink" should {
+    "write csv data" in {
+
+      val schema = Schema(Field("name"), Field("job"), Field("location"))
+      val frame = Frame(
+        schema,
+        Row(schema, Vector("clint eastwood", "actor", "carmel")),
+        Row(schema, Vector("elton john", "musician", "pinner"))
+      )
+
+      val temp = Files.createTempFile("csvsink", ".test")
+      frame.to(CsvSink(temp))
+      val result = Files.readAllLines(temp, Charset.defaultCharset()).asScala.mkString("\n")
+      result shouldBe "clint eastwood,actor,carmel\nelton john,musician,pinner"
+    }
+    "support setting delimiter" in {
+
+      val schema = Schema(Field("name"), Field("job"), Field("location"))
+      val frame = Frame(
+        schema,
+        Row(schema, Vector("clint eastwood", "actor", "carmel")),
+        Row(schema, Vector("elton john", "musician", "pinner"))
+      )
+
+      val temp = Files.createTempFile("csvsink", ".test")
+      frame.to(CsvSink(temp, format = CsvFormat().copy(delimiter = '>')))
+      val result = Files.readAllLines(temp, Charset.defaultCharset()).asScala.mkString("\n")
+      result shouldBe "clint eastwood>actor>carmel\nelton john>musician>pinner"
+    }
+    "write headers when specified" in {
+      val schema = Schema(Field("name"), Field("job"), Field("location"))
+      val frame = Frame(
+        schema,
+        Row(schema, Vector("clint eastwood", "actor", "carmel")),
+        Row(schema, Vector("elton john", "musician", "pinner"))
+      )
+      val temp = Files.createTempFile("csvsink", ".test")
+      frame.to(CsvSink(temp).withHeaders(Header.FirstRow))
+      val result = Files.readAllLines(temp, Charset.defaultCharset()).asScala.mkString("\n")
+      result shouldBe "name,job,location\nclint eastwood,actor,carmel\nelton john,musician,pinner"
+    }
+    "write null values as empty strings" in {
+      val schema = Schema(Field("name"), Field("job"), Field("location"))
+      val frame = Frame(
+        schema,
+        Row(schema, Vector("clint eastwood", null, "carmel")),
+        Row(schema, Vector("elton john", null, "pinner"))
+      )
+      val temp = Files.createTempFile("csvsink", ".test")
+      frame.to(CsvSink(temp))
+      val result = Files.readAllLines(temp, Charset.defaultCharset()).asScala.mkString("\n")
+      result shouldBe "clint eastwood,,carmel\nelton john,,pinner"
+    }
+  }
+}
---
+++ eel-core/src/test/scala/io/eels/component/csv/CsvSourceTest.scala
@@ -0,0 +1,93 @@
+package io.eels.component.csv
+
+import java.nio.file.Paths
+
+import io.eels.schema.{Field, FieldType, Schema}
+import org.scalatest.{Matchers, WordSpec}
+
+class CsvSourceTest extends WordSpec with Matchers {
+
+  "CsvSource" should {
+    "read schema" in {
+      val file = getClass.getResource("/csvtest.csv").toURI()
+      val path = Paths.get(file)
+      CsvSource(path).schema() shouldBe Schema(
+        Field("a", FieldType.String, true),
+        Field("b", FieldType.String, true),
+        Field("c", FieldType.String, true)
+      )
+    }
+    "support null cell value option as null" in {
+      val file = getClass.getResource("/csvwithempty.csv").toURI()
+      val path = Paths.get(file)
+      CsvSource(path).withNullValue(null).toFrame(1).toSet().map(_.values).toSet shouldBe
+        Set(Vector("1", null, "3"))
+    }
+    "support null cell value replacement value" in {
+      val file = getClass.getResource("/csvwithempty.csv").toURI()
+      val path = Paths.get(file)
+      CsvSource(path).withNullValue("foo").toFrame(1).toSet().map(_.values).toSet shouldBe
+        Set(Vector("1", "foo", "3"))
+    }
+    "read from path" in {
+      val file = getClass.getResource("/csvtest.csv").toURI()
+      val path = Paths.get(file)
+      CsvSource(path).withHeader(Header.FirstRow).toFrame(1).size() shouldBe 3
+      CsvSource(path).withHeader(Header.None).toFrame(1).size() shouldBe 4
+    }
+    "allow specifying manual schema" in {
+      val file = getClass.getResource("/csvtest.csv").toURI()
+      val path = Paths.get(file)
+      val schema = Schema(
+        Field("test1", FieldType.String, true),
+        Field("test2", FieldType.String, true),
+        Field("test3", FieldType.String, true)
+      )
+      CsvSource(path).withSchema(schema).toFrame(1).schema() shouldBe schema
+    }
+    "support reading header" in {
+      val file = getClass.getResource("/csvtest.csv").toURI()
+      val path = Paths.get(file)
+      CsvSource(path).withHeader(Header.FirstRow).toFrame(1).toList().map(_.values).toSet shouldBe
+        Set(Vector("e", "f", "g"), Vector("1", "2", "3"), Vector("4", "5", "6"))
+    }
+    "support skipping header" in {
+      val file = getClass.getResource("/csvtest.csv").toURI()
+      val path = Paths.get(file)
+      CsvSource(path).withHeader(Header.None).toFrame(1).toSet().map(_.values) shouldBe
+        Set(Vector("a", "b", "c"), Vector("e", "f", "g"), Vector("1", "2", "3"), Vector("4", "5", "6"))
+    }
+    "support delimiters" in {
+      val file = getClass.getResource("/psv.psv").toURI()
+      val path = Paths.get(file)
+      CsvSource(path).withDelimiter('|').toFrame(1).toList().map(_.values).toSet shouldBe
+        Set(Vector("e", "f", "g"))
+      CsvSource(path).withDelimiter('|').withHeader(Header.None).toFrame(1).toSet().map(_.values) shouldBe
+        Set(Vector("a", "b", "c"), Vector("e", "f", "g"))
+    }
+    "support comments for headers" in {
+      val file = getClass.getResource("/comments.csv").toURI()
+      val path = Paths.get(file)
+      CsvSource(path).withHeader(Header.FirstComment).schema() shouldBe Schema(
+        Field("a", FieldType.String, true),
+        Field("b", FieldType.String, true),
+        Field("c", FieldType.String, true)
+      )
+      CsvSource(path).withHeader(Header.FirstComment).toFrame(1).toSet().map(_.values) shouldBe
+        Set(Vector("1", "2", "3"), Vector("e", "f", "g"), Vector("4", "5", "6"))
+    }
+    "terminate if asking for first comment but no comments" in {
+      val file = getClass.getResource("/csvtest.csv").toURI()
+      val path = Paths.get(file)
+      CsvSource(path).withHeader(Header.FirstComment).schema() shouldBe Schema(
+        Field("", FieldType.String, true)
+      )
+    }
+    "support verifying rows" in {
+      val file = getClass.getResource("/corrupt.csv").toURI()
+      val path = Paths.get(file)
+      CsvSource(path).withHeader(Header.FirstRow).toFrame(1).toList().map(_.values) shouldBe
+        Seq(Vector("1", "2", "3"))
+    }
+  }
+}
