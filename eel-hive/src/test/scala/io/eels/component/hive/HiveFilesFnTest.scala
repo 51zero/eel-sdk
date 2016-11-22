4e8c354792ede4b7a3a752ee1e5f8e43d0033d56
sksamuel <sam@sksamuel.com>
2016-11-22 23:51:25 +0000

    Removed hive test kit

--- eel-hive/src/test/scala/io/eels/component/hive/HiveFilesFnTest.scala
+++ eel-hive/src/test/scala/io/eels/component/hive/HiveFilesFnTest.scala
@@ -1,35 +1,35 @@
-package io.eels.component.hive
-
-import io.eels.{Frame, Row}
-import io.eels.schema.Schema
-import io.eels.testkit.HiveTestKit
-import org.scalatest.{Matchers, WordSpec}
-
-class HiveFilesFnTest extends WordSpec with Matchers with HiveTestKit {
-
-  val schema = Schema("a", "b")
-  val data = Seq.fill(1000)(Seq(Row(schema, "1", "2"), Row(schema, "2", "3"), Row(schema, "3", "4"))).flatten
-
-  "HiveFilesFn" should {
-    "scan table root on non partitioned table" ignore {
-
-      new HiveOps(client).createTable("sam", "hivefilesfn1", schema, Nil, format = HiveFormat.Parquet)
-      Frame(schema, data).to(HiveSink("sam", "hivefilesfn1").withIOThreads(2))
-
-      val table = client.getTable("sam", "hivefilesfn1")
-
-      // we should have 2 files, one per thread, as data is written straight to the table root
-      HiveFilesFn(table, Nil).size shouldBe 2
-    }
-    "scan partition paths for partitioned table" ignore {
-
-      new HiveOps(client).createTable("sam", "hivefilesfn2", schema, partitionKeys = List("a"), HiveFormat.Parquet)
-      Frame(schema, data).to(HiveSink("sam", "hivefilesfn2").withIOThreads(2))
-
-      val table = client.getTable("sam", "hivefilesfn2")
-
-      // we should have 6 files, 3 partition values, and then 2 files for each partition (2 threads)
-      HiveFilesFn(table, Nil).size shouldBe 6
-    }
-  }
-}
+//package io.eels.component.hive
+//
+//import io.eels.{Frame, Row}
+//import io.eels.schema.Schema
+//import io.eels.testkit.HiveTestKit
+//import org.scalatest.{Matchers, WordSpec}
+//
+//class HiveFilesFnTest extends WordSpec with Matchers with HiveTestKit {
+//
+//  val schema = Schema("a", "b")
+//  val data = Seq.fill(1000)(Seq(Row(schema, "1", "2"), Row(schema, "2", "3"), Row(schema, "3", "4"))).flatten
+//
+//  "HiveFilesFn" should {
+//    "scan table root on non partitioned table" ignore {
+//
+//      new HiveOps(client).createTable("sam", "hivefilesfn1", schema, Nil, format = HiveFormat.Parquet)
+//      Frame(schema, data).to(HiveSink("sam", "hivefilesfn1").withIOThreads(2))
+//
+//      val table = client.getTable("sam", "hivefilesfn1")
+//
+//      // we should have 2 files, one per thread, as data is written straight to the table root
+//      HiveFilesFn(table, Nil).size shouldBe 2
+//    }
+//    "scan partition paths for partitioned table" ignore {
+//
+//      new HiveOps(client).createTable("sam", "hivefilesfn2", schema, partitionKeys = List("a"), HiveFormat.Parquet)
+//      Frame(schema, data).to(HiveSink("sam", "hivefilesfn2").withIOThreads(2))
+//
+//      val table = client.getTable("sam", "hivefilesfn2")
+//
+//      // we should have 6 files, 3 partition values, and then 2 files for each partition (2 threads)
+//      HiveFilesFn(table, Nil).size shouldBe 6
+//    }
+//  }
+//}
--- eel-hive/src/test/scala/io/eels/component/hive/HiveOpsTest.scala
+++ eel-hive/src/test/scala/io/eels/component/hive/HiveOpsTest.scala
@@ -1,70 +1,70 @@
-package io.eels.component.hive
-
-import java.util
-
-import io.eels.schema.{Field, FieldType, Schema}
-import io.eels.testkit.HiveTestKit
-import org.apache.hadoop.hive.metastore.api.{Database, FieldSchema, StorageDescriptor, Table}
-import org.scalatest.{Matchers, WordSpec}
-
-import scala.collection.JavaConverters._
-
-class HiveOpsTest extends WordSpec with Matchers with HiveTestKit {
-
-  "HiveOps.tableFormat" should {
-    "return table format from hive meta store" in {
-
-      val table = new Table()
-      table.setDbName("db")
-      table.setTableName("tywin")
-      table.setSd(new StorageDescriptor())
-      table.getSd.setInputFormat("testformat")
-
-      client.createDatabase(new Database("db", "", "", new util.HashMap))
-      client.createTable(table)
-
-      new HiveOps(client).tableFormat("db", "tywin") shouldBe "testformat"
-    }
-  }
-
-  "HiveOps.location" should {
-    "return table location from hive meta store" in {
-
-      val table = new Table()
-      table.setDbName("db")
-      table.setTableName("brianne")
-      table.setSd(new StorageDescriptor())
-      table.getSd.setLocation("mytestlocation")
-      table.getSd.setInputFormat("testformat")
-
-      client.createDatabase(new Database("db", "", "", new util.HashMap))
-      client.createTable(table)
-
-      new HiveOps(client).location("db", "brianne") shouldBe "mytestlocation"
-    }
-  }
-
-  "HiveOps.schema" should {
-    "set fields to non null and set partitions to non null" in {
-
-      val table = new Table()
-      table.setDbName("db")
-      table.setTableName("jaime")
-      table.setSd(new StorageDescriptor())
-      table.getSd.setInputFormat("testformat")
-      table.getSd.setCols(List(new FieldSchema("p", "string", null), new FieldSchema("q", "string", null)).asJava)
-      table.setPartitionKeys(List(new FieldSchema("a", "string", null), new FieldSchema("b", "string", null)).asJava)
-
-      client.createDatabase(new Database("db", "", "", new util.HashMap))
-      client.createTable(table)
-
-      new HiveOps(client).schema("db", "jaime") shouldBe
-        Schema(
-          Field("p", FieldType.String, nullable = true),
-          Field("q", FieldType.String, nullable = true),
-          Field("a", FieldType.String, nullable = false, partition = true),
-          Field("b", FieldType.String, nullable = false, partition = true)
-        )
-    }
-  }
-}
+//package io.eels.component.hive
+//
+//import java.util
+//
+//import io.eels.schema.{Field, FieldType, Schema}
+//import io.eels.testkit.HiveTestKit
+//import org.apache.hadoop.hive.metastore.api.{Database, FieldSchema, StorageDescriptor, Table}
+//import org.scalatest.{Matchers, WordSpec}
+//
+//import scala.collection.JavaConverters._
+//
+//class HiveOpsTest extends WordSpec with Matchers with HiveTestKit {
+//
+//  "HiveOps.tableFormat" should {
+//    "return table format from hive meta store" in {
+//
+//      val table = new Table()
+//      table.setDbName("db")
+//      table.setTableName("tywin")
+//      table.setSd(new StorageDescriptor())
+//      table.getSd.setInputFormat("testformat")
+//
+//      client.createDatabase(new Database("db", "", "", new util.HashMap))
+//      client.createTable(table)
+//
+//      new HiveOps(client).tableFormat("db", "tywin") shouldBe "testformat"
+//    }
+//  }
+//
+//  "HiveOps.location" should {
+//    "return table location from hive meta store" in {
+//
+//      val table = new Table()
+//      table.setDbName("db")
+//      table.setTableName("brianne")
+//      table.setSd(new StorageDescriptor())
+//      table.getSd.setLocation("mytestlocation")
+//      table.getSd.setInputFormat("testformat")
+//
+//      client.createDatabase(new Database("db", "", "", new util.HashMap))
+//      client.createTable(table)
+//
+//      new HiveOps(client).location("db", "brianne") shouldBe "mytestlocation"
+//    }
+//  }
+//
+//  "HiveOps.schema" should {
+//    "set fields to non null and set partitions to non null" in {
+//
+//      val table = new Table()
+//      table.setDbName("db")
+//      table.setTableName("jaime")
+//      table.setSd(new StorageDescriptor())
+//      table.getSd.setInputFormat("testformat")
+//      table.getSd.setCols(List(new FieldSchema("p", "string", null), new FieldSchema("q", "string", null)).asJava)
+//      table.setPartitionKeys(List(new FieldSchema("a", "string", null), new FieldSchema("b", "string", null)).asJava)
+//
+//      client.createDatabase(new Database("db", "", "", new util.HashMap))
+//      client.createTable(table)
+//
+//      new HiveOps(client).schema("db", "jaime") shouldBe
+//        Schema(
+//          Field("p", FieldType.String, nullable = true),
+//          Field("q", FieldType.String, nullable = true),
+//          Field("a", FieldType.String, nullable = false, partition = true),
+//          Field("b", FieldType.String, nullable = false, partition = true)
+//        )
+//    }
+//  }
+//}
