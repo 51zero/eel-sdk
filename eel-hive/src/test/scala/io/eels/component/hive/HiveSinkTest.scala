a808587c13f482c66fa015dda5134bb1db942c1d
sksamuel <sam@sksamuel.com>
2016-11-23 01:07:21 +0000

    Fixed build

--- eel-hive/src/test/scala/io/eels/component/hive/HiveSinkTest.scala
+++ eel-hive/src/test/scala/io/eels/component/hive/HiveSinkTest.scala
@@ -1,8 +1,6 @@
 package io.eels.component.hive
 
-import io.eels.Frame
 import io.eels.schema.Schema
-import org.apache.commons.io.IOUtils
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.hadoop.hive.conf.HiveConf
 import org.apache.hadoop.hive.metastore.HiveMetaStoreClient
@@ -33,33 +31,32 @@ class HiveSinkTest extends WordSpec with Matchers with Inspectors {
   }
 
 
-  "HiveSink" should {
-    "not write partition data to the data file" ignore {
-
-      val tableName = "hivesinktest" + System.currentTimeMillis()
-
-      val schema = Schema("a", "b")
-      ops.createTable("sam", tableName, schema, format = HiveFormat.Parquet, partitionKeys = List("b"))
-
-      val frame = Frame.fromValues(schema, List.fill(100)(List("HOLLY", "QUEEG")))
-      frame.to(HiveSink("sam", tableName))
-
-      val table = client.getTable("sam", tableName)
-      val paths = HiveFilesFn(table, List("b")).map(_._1.getPath)
-
-      // none of these files should contain col b as that's a partition
-      forExactly(0, paths) { it =>
-        val ins = fs.open(it)
-        val str = IOUtils.toString(ins)
-        str should include("QUEEG")
-      }
-
-      // all files should contain col a as that's not a partition
-      forAll(paths) { it =>
-        val ins = fs.open(it)
-        val str = IOUtils.toString(ins)
-        str should include("HOLLY")
-      }
-    }
-  }
+//  "HiveSink" should {
+//    "not write partition data to the data file" ignore {
+//
+//      val tableName = "hivesinktest" + System.currentTimeMillis()
+//
+//      val schema = Schema("a", "b")
+//      ops.createTable("sam", tableName, schema, format = HiveFormat.Parquet, partitionKeys = List("b"))
+//
+//      val frame = Frame.fromValues(schema, List.fill(100)(List("HOLLY", "QUEEG")))
+//      frame.to(HiveSink("sam", tableName))
+//
+//      val table = client.getTable("sam", tableName)
+//      val paths = HiveFilesFn(table, List("b")).map(_._1.getPath)
+//
+//      // none of these files should contain col b as that's a partition
+//      forExactly(0, paths) { it =>
+//        val ins = fs.open(it)
+//        val str = IOUtils.toString(ins)
+//        str should include("QUEEG")
+//      }
+//
+//      // all files should contain col a as that's not a partition
+//      forAll(paths) { it =>
+//        val ins = fs.open(it)
+//        val str = IOUtils.toString(ins)
+//        str should include("HOLLY")
+//      }
+//    }
 }
