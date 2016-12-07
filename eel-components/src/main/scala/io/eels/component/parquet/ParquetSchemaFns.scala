643e0ce8c6b0b3278ebbdef0a3b0278a3d2889c8
sksamuel <sam@sksamuel.com>
2016-12-07 12:18:02 +0000

    Added repetition values to parquet schema fn

--- eel-components/src/main/scala/io/eels/component/parquet/ParquetSchemaFns.scala
+++ eel-components/src/main/scala/io/eels/component/parquet/ParquetSchemaFns.scala
@@ -7,19 +7,22 @@ import org.apache.parquet.schema._
 
 object ParquetSchemaFns {
 
-  def toParquetType(field: Field): Type = field.dataType match {
-    case BigIntType => new PrimitiveType(Repetition.REQUIRED, PrimitiveTypeName.INT96, field.name)
-    case BooleanType => new PrimitiveType(Repetition.REQUIRED, PrimitiveTypeName.BOOLEAN, field.name)
-    case DateType => new PrimitiveType(Repetition.REQUIRED, PrimitiveTypeName.BINARY, field.name)
-    case DecimalType(precision, scale) => new PrimitiveType(Repetition.REQUIRED, PrimitiveTypeName.DOUBLE, field.name)
-    case DoubleType => new PrimitiveType(Repetition.REQUIRED, PrimitiveTypeName.DOUBLE, field.name)
-    case FloatType => new PrimitiveType(Repetition.REQUIRED, PrimitiveTypeName.DOUBLE, field.name)
-    case i: IntType => new PrimitiveType(Repetition.REQUIRED, PrimitiveTypeName.INT32, field.name)
-    case l: LongType => new PrimitiveType(Repetition.REQUIRED, PrimitiveTypeName.INT64, field.name)
-    case ShortType => new PrimitiveType(Repetition.REQUIRED, PrimitiveTypeName.INT32, field.name)
-    case StructType(fields) => new GroupType(Repetition.REQUIRED, field.name, fields.map(toParquetType): _*)
-    case StringType => new PrimitiveType(Repetition.REQUIRED, PrimitiveTypeName.BINARY, field.name, OriginalType.UTF8)
-    case TimestampType => new PrimitiveType(Repetition.REQUIRED, PrimitiveTypeName.INT96, field.name, OriginalType.TIMESTAMP_MILLIS)
+  def toParquetType(field: Field): Type = {
+    val repetition = if (field.nullable) Repetition.OPTIONAL else Repetition.REQUIRED
+    field.dataType match {
+      case BigIntType => new PrimitiveType(repetition, PrimitiveTypeName.INT96, field.name)
+      case BooleanType => new PrimitiveType(repetition, PrimitiveTypeName.BOOLEAN, field.name)
+      case DateType => new PrimitiveType(repetition, PrimitiveTypeName.BINARY, field.name)
+      case DecimalType(precision, scale) => new PrimitiveType(Repetition.REQUIRED, PrimitiveTypeName.DOUBLE, field.name, OriginalType.DECIMAL)
+      case DoubleType => new PrimitiveType(repetition, PrimitiveTypeName.DOUBLE, field.name)
+      case FloatType => new PrimitiveType(repetition, PrimitiveTypeName.FLOAT, field.name)
+      case i: IntType => new PrimitiveType(repetition, PrimitiveTypeName.INT32, field.name)
+      case l: LongType => new PrimitiveType(repetition, PrimitiveTypeName.INT64, field.name)
+      case ShortType => new PrimitiveType(repetition, PrimitiveTypeName.INT32, field.name)
+      case StructType(fields) => new GroupType(repetition, field.name, fields.map(toParquetType): _*)
+      case StringType => new PrimitiveType(repetition, PrimitiveTypeName.BINARY, field.name, OriginalType.UTF8)
+      case TimestampType => new PrimitiveType(repetition, PrimitiveTypeName.INT96, field.name, OriginalType.TIMESTAMP_MILLIS)
+    }
   }
 
   def toParquetSchema(schema: StructType): MessageType = {
