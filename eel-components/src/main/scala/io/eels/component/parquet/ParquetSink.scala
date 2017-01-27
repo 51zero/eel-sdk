1782b63e214ec07274d2cef467721e8d36bf4051
sksamuel <sam@sksamuel.com>
2017-01-27 03:36:06 +0000

    Added permissions to #226

--- eel-components/src/main/scala/io/eels/component/parquet/ParquetSink.scala
+++ eel-components/src/main/scala/io/eels/component/parquet/ParquetSink.scala
@@ -3,15 +3,20 @@ package io.eels.component.parquet
 import com.sksamuel.exts.Logging
 import io.eels.schema.StructType
 import io.eels.{Row, Sink, SinkWriter}
+import org.apache.hadoop.fs.permission.FsPermission
 import org.apache.hadoop.fs.{FileSystem, Path}
 
 case class ParquetSink(path: Path,
                        overwrite: Boolean = false,
+                       permission: Option[FsPermission] = None,
+                       inheritPermissions: Option[Boolean] = None,
                        metadata: Map[String, String] = Map.empty)
                       (implicit fs: FileSystem) extends Sink with Logging {
 
   def withMetaData(map: Map[String, String]): ParquetSink = copy(metadata = map)
   def withOverwrite(overwrite: Boolean): ParquetSink = copy(overwrite = overwrite)
+  def withPermission(permission: FsPermission): ParquetSink = copy(permission = Option(permission))
+  def withInheritPermission(inheritPermissions: Boolean): ParquetSink = copy(inheritPermissions = Option(inheritPermissions))
 
   override def writer(schema: StructType): SinkWriter = new SinkWriter {
 
@@ -26,6 +31,14 @@ case class ParquetSink(path: Path,
 
     override def close(): Unit = {
       writer.close()
+      permission match {
+        case Some(permission) => fs.setPermission(path, permission)
+        case None =>
+          if (inheritPermissions.getOrElse(false)) {
+            val permission = fs.getFileStatus(path.getParent).getPermission
+            fs.setPermission(path, permission)
+          }
+      }
     }
   }
 }
