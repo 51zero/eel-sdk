25b291b3f0a31c0574ebad20eec32d6990487131
sksamuel <sam@sksamuel.com>
2017-06-05 07:49:24 +0100

    Fixed test for release

--- eel-components/src/test/scala/io/eels/component/jdbc/BucketPartitionStrategyTest.scala
+++ eel-components/src/test/scala/io/eels/component/jdbc/BucketPartitionStrategyTest.scala
@@ -8,10 +8,10 @@ import scala.util.Random
 
 class BucketPartitionStrategyTest extends WordSpec with Matchers {
 
-  private val conn = DriverManager.getConnection("jdbc:h2:mem:bucket_test")
-  conn.createStatement().executeUpdate("create table mytable (a integer)")
+  private val conn = DriverManager.getConnection("jdbc:h2:mem:test")
+  conn.createStatement().executeUpdate("create table bucket_test (a integer)")
   for (k <- 0 until 20) {
-    conn.createStatement().executeUpdate(s"insert into mytable (a) values (${Random.nextInt(10000)})")
+    conn.createStatement().executeUpdate(s"insert into bucket_test (a) values (${Random.nextInt(10000)})")
   }
 
   "BucketPartitionStrategy" should {
@@ -23,12 +23,12 @@ class BucketPartitionStrategyTest extends WordSpec with Matchers {
       BucketPartitionStrategy("a", 6, 1, 29).ranges shouldBe List(Range.inclusive(1, 5), Range.inclusive(6, 10), Range.inclusive(11, 15), Range.inclusive(16, 20), Range.inclusive(21, 25), Range.inclusive(26, 29))
     }
     "return correct number of ranges" in {
-      JdbcSource(() => conn, "select * from mytable")
+      JdbcSource(() => conn, "select * from bucket_test")
         .withPartitionStrategy(BucketPartitionStrategy("a", 4, 0, 10000))
         .parts().size shouldBe 4
     }
     "return full and non overlapping data" in {
-      JdbcSource(() => conn, "select * from mytable")
+      JdbcSource(() => conn, "select * from bucket_test")
         .withPartitionStrategy(BucketPartitionStrategy("a", 4, 0, 10000))
         .toFrame().collect().size shouldBe 20
     }
