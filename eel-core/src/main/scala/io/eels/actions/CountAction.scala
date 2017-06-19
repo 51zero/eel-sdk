1cc5f81895392dafa194b3369a7286cb63203d53
sksamuel <sam@sksamuel.com>
2017-06-20 00:15:56 +0100

    Parallelized .size

--- eel-core/src/main/scala/io/eels/actions/CountAction.scala
+++ eel-core/src/main/scala/io/eels/actions/CountAction.scala
@@ -1,7 +1,29 @@
 package io.eels.actions
 
+import java.util.concurrent.atomic.LongAdder
+import java.util.concurrent.{CountDownLatch, TimeUnit}
+
 import io.eels.datastream.DataStream
 
+import scala.concurrent.ExecutionContext
+
 case class CountAction(ds: DataStream) extends Action {
-  def execute: Long = ds.partitions.foldLeft(0) { (count, ci) => count + ci.iterator.size }
+
+  private val executor = ExecutionContext.Implicits.global
+
+  def execute: Long = {
+    val adder = new LongAdder
+    val partitions = ds.partitions
+    val latch = new CountDownLatch(partitions.size)
+    partitions.foreach { partition =>
+      executor.execute(new Runnable {
+        override def run(): Unit = {
+          partition.iterator.foreach(_ => adder.add(1))
+          latch.countDown()
+        }
+      })
+    }
+    latch.await(1, TimeUnit.DAYS)
+    adder.sum()
+  }
 }
