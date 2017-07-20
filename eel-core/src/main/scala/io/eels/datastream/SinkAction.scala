fe64efce827f547b7e61545bc8b6f092976e579e
sksamuel <sam@sksamuel.com>
2017-07-20 12:37:19 +0100

    Fixed memory leak in sink action #325

--- eel-core/src/main/scala/io/eels/datastream/SinkAction.scala
+++ eel-core/src/main/scala/io/eels/datastream/SinkAction.scala
@@ -1,12 +1,10 @@
 package io.eels.datastream
 
-import java.util.concurrent.atomic.LongAdder
-import java.util.concurrent.{ConcurrentLinkedQueue, Executors, TimeUnit}
+import java.util.concurrent.atomic.{AtomicLong, LongAdder}
+import java.util.concurrent.{Executors, LinkedBlockingQueue, TimeUnit}
 
 import com.sksamuel.exts.Logging
-import io.eels.{Row, SinkWriter, Sink}
-
-import scala.collection.JavaConverters._
+import io.eels.{Row, Sink}
 
 case class SinkAction(ds: DataStream, sink: Sink, parallelism: Int) extends Logging {
 
@@ -17,42 +15,57 @@ case class SinkAction(ds: DataStream, sink: Sink, parallelism: Int) extends Logg
 
     var failure: Throwable = null
 
-    val writers = new ConcurrentLinkedQueue[SinkWriter](sink.open(schema, parallelism).asJava)
     val executor = Executors.newFixedThreadPool(parallelism)
+    val queue = new LinkedBlockingQueue[Seq[Row]](100)
+    val completed = new AtomicLong(0)
 
-    class WriteTask(chunk: Seq[Row]) extends Runnable {
-      override def run(): Unit = {
-        // this cannot return null, as we created enough writers for everyone
-        val writer = writers.poll()
-        try {
-          chunk.foreach { row =>
-            writer.write(row)
-            adder.increment()
+    val writers = sink.open(schema, parallelism)
+    writers.zipWithIndex.foreach { case (writer, k) =>
+      executor.submit(new Runnable {
+        override def run(): Unit = {
+          logger.info(s"Starting thread writer $k")
+          while (true) {
+            val chunk = queue.take()
+            try {
+              chunk.foreach { row =>
+                writer.write(row)
+                adder.increment()
+              }
+            } catch {
+              case t: Throwable =>
+                logger.error("Error writing to stream", t)
+                // if we have an error writing, we'll exit all writers immediately
+                executor.shutdownNow()
+                failure = t
+            } finally {
+              logger.debug(s"Chunk ${completed.incrementAndGet} has completed")
+            }
           }
-        } catch {
-          case t: Throwable =>
-            logger.error("Error writing to stream", t)
-            // if we have an error writing, we'll exit immediately
-            executor.shutdownNow()
-            failure = t
-        } finally {
-          writers.add(writer)
         }
-      }
+      })
     }
+    executor.shutdown()
 
     ds.subscribe(new Subscriber[Seq[Row]] {
-      override def starting(s: Cancellable): Unit = ()
-      override def next(chunk: Seq[Row]): Unit = {
-        // each time our subscriber gets a chunk it can queue a task to process that chunk
-        // avoiding the need for a blocking queue
-        executor.submit(new WriteTask(chunk))
+
+      var cancellable: Cancellable = null
+
+      override def starting(c: Cancellable): Unit = {
+        logger.debug(s"Subscribing to datastream for sink action [cancellable=$c]")
+        this.cancellable = c
       }
+
+      override def next(chunk: Seq[Row]): Unit = queue.put(chunk)
+
       override def completed(): Unit = {
-        executor.shutdown()
+        logger.debug("Sink action has received all chunks")
+        queue.put(Row.Sentinel)
       }
+
       override def error(t: Throwable): Unit = {
-        // if we have an error from downstream, we'll exit immediately
+        // if we have an error from downstream, we'll exit immediately and cancel downstream
+        if (cancellable != null)
+          cancellable.cancel()
         executor.shutdownNow()
         failure = t
       }
@@ -65,7 +78,7 @@ case class SinkAction(ds: DataStream, sink: Sink, parallelism: Int) extends Logg
 
     // we close all writers together so its atomic-ish
     logger.info("Closing all sink writers...")
-    writers.asScala.foreach(_.close)
+    writers.foreach(_.close)
     logger.info("All sink writers are closed")
 
     if (failure != null)
