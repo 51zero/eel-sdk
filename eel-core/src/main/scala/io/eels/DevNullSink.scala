3d40874fbcb0000cf220c1a9f8f645c218502e63
sksamuel <sam@sksamuel.com>
2017-07-23 23:36:50 +0100

    Add dev/null style sink to test source speeds #328

---
+++ eel-core/src/main/scala/io/eels/DevNullSink.scala
@@ -0,0 +1,14 @@
+package io.eels
+
+import io.eels.schema.StructType
+
+/**
+  * A sink which just swallows any incoming data.
+  * Useful to test throughput speeds of sources.
+  */
+object DevNullSink extends Sink {
+  override def open(schema: StructType): SinkWriter = new SinkWriter {
+    override def close(): Unit = ()
+    override def write(row: Row): Unit = ()
+  }
+}
